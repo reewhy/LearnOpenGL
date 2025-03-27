b33cfb6a14c615beabd15a48ebc35d1dcab4a791
Junhici <lucasmegaX0501@gmaill.com>
2025-03-27 22:52:51 +0100

    Window created and input received

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -2,6 +2,15 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// Callback for handling window resizing
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// Close on "space" press
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
+	if (glfwGetKey(window, GLFW_KEY_0) == GLFW_PRESS) std::cout << "Pressed" << std::endl;
+}
+
 int main() {
 	glfwInit();
 	// Window configuration options
@@ -19,5 +28,36 @@ int main() {
 	}
 	// Making the window the main context of the current thread
 	glfwMakeContextCurrent(window);
+	
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Setting size of the rendering window
+	// (with this function OpenGL knows that we want to display data and coordinates with respect to the window)
+	glViewport(0, 0, 800, 600);
+
+	// Setting window resize callback function
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	// Render loop
+	while (!glfwWindowShouldClose(window)) {
+		// Input
+		processInput(window);
+
+		// Rendering
+
+		// Check and call  events and swap the buffers
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	// Clean GLFW's resources
+	glfwTerminate();
 	return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
 }

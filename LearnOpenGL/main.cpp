301879ff76da7f4110849f62796d53f6d7ea46b3
reewhy <moretti.luca0501@gmail.com>
2025-03-29 15:33:31 +0100

    Updates

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -2,6 +2,9 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 #include "Shader.h"
+#include <filesystem>
+#define STB_IMAGE_IMPLEMENTATION
+#include "libs/stb_image.h"
 
 // Window size
 const unsigned int SCR_WIDTH = 800;
@@ -20,6 +23,7 @@ void changeMode();
 void process_input_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
 int main() {
+    std::cout << "Current working directory: " << std::filesystem::current_path() << std::endl;
 	// Initilize
 	glfwInit();
 	// Window configuration options
@@ -48,23 +52,40 @@ int main() {
 		return -1;
 	}
 
-	Shader ourShader("shader.vs", "shader.fs");
-
+	// TEST SHADER
+	// Shader ourShader("../vertexTest.vs", "../fragmentTest.fs");
+	Shader ourShader("../shader.vs", "../shader.fs");
 	// Vertex array
 	float vertices[] = {
-		// positions			// colors
-		 0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // bottom right
-		-0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // bottom left
-		 0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // top 
+		// positions          // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+	   -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+	   -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
+   };
+
+	unsigned int indices[] = {
+		0, 1, 3,
+		1, 2, 3
 	};
+
+	// Texture coordinates
+	float texCoords[] = {
+		0.0f, 0.0f,
+		1.0f, 0.0f,
+		0.5f, 1.0f
+	};
+
 
 	// Creating a VAO (Vertex Arrat Objects buffer)
 	unsigned int VAO;
 	// Buffer of object
 	unsigned int VBO;
+	unsigned int EBO;
 	glGenVertexArrays(1, &VAO);
 	// Giving an ID to the buffer
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 	// Bind the vertex array
 	glBindVertexArray(VAO);
 	// Binding the buffe
@@ -72,13 +93,21 @@ int main() {
 	// Copying the verteces in the buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	// Telling OpenGL how to interpret vertex data
 	// position attribute
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	// color attribute
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3* sizeof(float)));
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3* sizeof(float)));
 	glEnableVertexAttribArray(1);
+	// test attribute
+	// glVertexAttribPointer(2, 1, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(6 * sizeof(float)));
+	// glEnableVertexAttribArray(2);
+	// texture coordinates attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	glBindVertexArray(VAO);
 
@@ -86,19 +115,63 @@ int main() {
 	// (with this function OpenGL knows that we want to display data and coordinates with respect to the window)
 	glViewport(0, 0, 800, 600);
 
+	// Texture object creation
+	unsigned int texture1, texture2;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(1);
+
+	unsigned char *data = stbi_load("../textures/container.jpg", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+	data = stbi_load("../textures/awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data) {
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	} else {
+		std::cout << "Failed to load texture" << std::endl;
+	}
+	stbi_image_free(data);
+
+	ourShader.use();
+	glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+
+
 	// Render loop
 	while (!glfwWindowShouldClose(window)) {
 		// Rendering
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
 		//	Activate program
-		ourShader.setFloat("offset", offset);
 		ourShader.use();
 		glBindVertexArray(VAO);
 		// glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 		//  DRAW THE FUCKING TRIANGLE(s)
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		// Check and call  events and swap the buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -106,6 +179,7 @@ int main() {
 	// Clean GLFW's resources
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glfwTerminate();
 	return 0;
 }
---
+++ LearnOpenGL/vertexTest.vs
@@ -0,0 +1,13 @@
+#version 330 core
+layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColor;
+layout (location = 2) in float aAlpha;
+
+uniform float offset;
+out vec4 ourColor;
+
+void main()
+{
+    gl_Position = vec4(aPos.x + offset, -aPos.y + aAlpha, aPos.z, 1.0f);
+    ourColor = vec4(aColor, 1.0f);
+}
